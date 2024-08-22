8bf8a933d4c8cff82b84f6ad594f40ea34d95159
iris-starry <iris_starry_@naver.com>
2024-08-22 11:19:07 +0900

    초기 커밋.

---
+++ WinFormAdv/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormAdv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 확대하기축소하기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 확대하기축소하기ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
